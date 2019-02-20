5c0f18c729ce447d1bfc34096942bf1900739894
Vishal Reniguntala <vishalhxc@gmail.com>
2019-02-19 23:09:53 -0600

    Added some visual changes and parralax scrolling.

--- VishalReniguntala.com/Gruntfile.js
+++ VishalReniguntala.com/Gruntfile.js
@@ -1,3 +1,23 @@
 module.exports = function(grunt) {
-
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/site.css': 'site.scss'                
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'site.sass',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['sass','watch']);
 };
